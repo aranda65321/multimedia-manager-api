5b82b236f9f463eb500a73d1c6f2336e80399115
juan65321 <juan.aranda@uao.edu.co>
2025-02-10 18:34:20 -0500

    codigo base

---
+++ src/main/java/com/co/multimedia/manager/MultimediaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.co.multimedia.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimediaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/multimedia/manager/MultimediaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.multimedia.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
